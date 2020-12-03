5457aee47cfffdbcc07455b0536df9a3054e599e
Xeolia <alexia.serrierb@gmail.com>
2020-12-03 18:33:26 +0100

    Installation

---
+++ src/main/java/com/la_cite/surfrider/SurfriderApplication.java
@@ -0,0 +1,13 @@
+package com.la_cite.surfrider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfriderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurfriderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/la_cite/surfrider/SurfriderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.la_cite.surfrider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfriderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
