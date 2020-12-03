fb3e34b0226ac2d665a710be5cf7cd00c883b21f
Guillaume Caugant <caugant@et.esiea.fr>
2020-12-03 20:15:05 +0100

    test

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+aze
 import java.net.*;
 import java.io.*;
 import java.nio.channels.*;
